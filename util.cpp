fd7cc65d2a0d1b83ba765b6787dee86b5559e350
mathyptr <mathyptr@gmail.com>
2025-12-24 17:01:01 +0100

    add util function

---
+++ util.cpp
@@ -0,0 +1,32 @@
+#include "util.h"
+#include <fstream>
+#include <filesystem> 
+#include <iostream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
+void SplashScreen() {
+    cout << "###############################################################"<<endl;
+
+    cout << "################################################################"<<endl;
+}
+
+void SplashResult() {
+    cout << "#####################################"<<endl;
+
+
+
+
+    cout << "#####################################"<<endl;
+
+}
+
+           
+
+
+
---
+++ util.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void SplashScreen();
+void SplashResult();
+
+#endif // UTIL_H
